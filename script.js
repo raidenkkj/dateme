e4647a018b9e57ed600f839886ec22561587015f
Raiden Ishigami <contact.raidenishi69@gmail.com>
2024-06-10 21:08:26 -0300

    implement audio
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Inicia o áudio de fundo
+    var bgAudio = document.getElementById('bg-audio');
+    bgAudio.play();
+
 var love = setInterval(function(){
   var r_num = Math.floor(Math.random() * 40) + 1;
 	var r_size = Math.floor(Math.random() * 65) + 10;
@@ -19,6 +23,10 @@ var love = setInterval(function(){
 },500);
 
 $(document).ready(function(){
+    // Inicia o áudio de fundo
+    var bgAudio = document.getElementById('bg-audio');
+    bgAudio.play();
+    
     var lines = [
         "Oi amor, tudo bem com você?",
         "Sei que já faz um tempo que eu já deveria ter te pedido isso...",
@@ -50,29 +58,22 @@ $(document).ready(function(){
         if (index < lines.length) {
             setTimeout(typeWriter, line.length * typingSpeed + 1000);
         } else {
-            // Remover todas as linhas anteriores
             $('.glass-card h2, .glass-card p').remove();
-            
-            // Adicionar apenas o último pedido de namoro
             $('.glass-card').append('<h2 id="line4">' + lines[3] + '</h2>');
-            
-            // Adicionar os botões somente após a exibição completa do texto
             setTimeout(function() {
                 $('.glass-card').append('<div id="buttons"><button class="btn btn-yes">Sim</button><button class="btn btn-no">Não</button></div>');
                 
-                // Evento de clique para o botão "Não"
+                var audioComemorando = document.getElementById('audio-comemorando');
+                var audioTristeza = document.getElementById('audio-tristeza');
+
                 $('.btn-no').click(function() {
                     console.log("Botão Não clicado");
-                    // Atualizar o texto do botão "Não"
+                    audioTristeza.play(); // Tocar o áudio tristeza.mp3
                     var nonoTexts = ["Certeza?", "Sério? :(", "Eu vou ficar tristão", "Muito tristão", "Aceita momoi", "Pufavo"];
                     var nonoIndex = 0;
                     
                     $(this).text(nonoTexts[nonoIndex]);
-                    
-                    // Remover o manipulador de eventos de clique anterior
                     $(this).off("click");
-                    
-                    // Adicionar o manipulador de eventos de clique novamente
                     $(this).click(function() {
                         console.log("Texto atualizado");
                         nonoIndex = (nonoIndex + 1) % nonoTexts.length;
@@ -80,43 +81,30 @@ $(document).ready(function(){
                     });
                 });
 
-                // Evento de clique para o botão "Sim"
-$('.btn-yes').click(function() {
-    console.log("Botão Sim clicado");
-    // Limpar o card e adicionar mensagem
-    $('.glass-card').empty().append('<h2>Agora eu e momo somos oficialmente namorados!!! Eu te amo minha princesa!!! ❤️</h2>');
+                $('.btn-yes').click(function() {
+                    console.log("Botão Sim clicado");
+                    audioComemorando.play(); // Tocar o áudio comemorando.mp3
+                    $('.glass-card').empty().append('<h2>Agora eu e momo somos oficialmente namorados!!! Eu te amo minha princesa!!! ❤️</h2>');
+                    $('.gif-container').removeClass('hidden');
+                    
+                    function addConfetti() {
+                        let params = {
+                            particleCount: 500,
+                            spread: 90,
+                            startVelocity: 70,
+                            origin: { x: 0, y: 0.5 },
+                            angle: 45
+                        };
+                        confetti(params);
+                        params.origin.x = 1;
+                        params.angle = 135;
+                        confetti(params);
+                    }
 
-    // Mostrar o GIF
-    $('.gif-container').removeClass('hidden');
-    
-    // Função para adicionar confete
-    function addConfetti() {
-        let params = {
-            particleCount: 500,
-            spread: 90,
-            startVelocity: 70,
-            origin: { x: 0, y: 0.5 },
-            angle: 45
-        };
-
-        // Joga confetes da esquerda pra direita
-        confetti(params);
-
-        // Joga confetes da direita para a esquerda
-        params.origin.x = 1;
-        params.angle = 135;
-        confetti(params);
-    }
-
-    // Adiciona o confete pela primeira vez
-    addConfetti();
-
-    // Adiciona o confete novamente após 2 segundos
-    setTimeout(addConfetti, 2000);
-
-    // Adiciona o confete mais uma vez após 4 segundos
-    setTimeout(addConfetti, 4000);
-});
+                    addConfetti();
+                    setTimeout(addConfetti, 2000);
+                    setTimeout(addConfetti, 4000);
+                });
             }, line.length * typingSpeed + 1000);
         }
     }
